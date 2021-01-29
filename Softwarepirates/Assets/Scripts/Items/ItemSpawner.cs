9b695c9c44d229abfd5400c061d7de5d6edc7c94
Marvin Baumann <marvin_baumann@hotmail.com>
2021-01-29 23:38:28 +0100

    Cloud System Update

--- Softwarepirates/Assets/Scripts/Items/ItemSpawner.cs
+++ Softwarepirates/Assets/Scripts/Items/ItemSpawner.cs
@@ -2,23 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CloudSpawner : MonoBehaviour
+public class ItemSpawner : MonoBehaviour
 {
     public GameObject cloudPrefab;
-    public Sprite[] cloudTextures;
     public float minWaitTime, maxWaitTime;
     public float minVertical, maxVertical;
     private GameObject holder;
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
+        Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, .1f);
     }
 
     void Start()
     {
-        holder = new GameObject("CloudHolder");
+        holder = new GameObject("ItemHolder");
         holder.transform.parent = transform;
         StartCoroutine(SpawnAndWait());
     }
@@ -32,9 +31,8 @@ public class CloudSpawner : MonoBehaviour
         CloudController controller = newCloud.GetComponent<CloudController>();
         controller.speed = 2 / transform.position.z;
         controller.SetKillPos(-transform.position.x);
-        newCloud.GetComponent<SpriteRenderer>().sprite = cloudTextures[Random.Range(0, cloudTextures.Length)];
         newCloud.transform.parent = holder.transform;
-        yield return new WaitForSeconds(Random.Range(minWaitTime/transform.position.z, maxWaitTime/transform.position.z));
+        yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
         StartCoroutine(SpawnAndWait());
     }
 }
