4e67f3d66fc23947172b4b1763ab2fe2cb2de6a1
TTobias <58746166+TTobias@users.noreply.github.com>
2021-01-29 21:29:20 +0100

    fixed stuff

--- Softwarepirates/Assets/Scripts/CloudSpawner.cs
+++ Softwarepirates/Assets/Scripts/CloudSpawner.cs
@@ -6,6 +6,7 @@ public class CloudSpawner : MonoBehaviour
 {
     public float cooldown = 1000f;
     public float tmp = 0f;
+    public GameObject cloud;
 
     private void FixedUpdate() {
         if(tmp <= 0f) {
@@ -18,6 +19,11 @@ public class CloudSpawner : MonoBehaviour
     }
 
     public void spawnCloud() {
-
+        GameObject g = Instantiate<GameObject>(cloud);
+        float depth = Random.Range(0f, 100f);
+        float height = Random.Range(0f, 100f);
+        cloud.transform.position = new Vector3(-200, height, -depth);
+        cloud.GetComponent<DriveByMovement>().depth = depth;
+        cloud.GetComponent<DriveByMovement>().speed = Random.Range(0.001f, 0.1f);
     }
 }
