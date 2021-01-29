1f6d8b253cc1bccd0333449e23dea03e1150f0f5
TTobias <58746166+TTobias@users.noreply.github.com>
2021-01-29 20:46:35 +0100

    added movmemt script

---
+++ Softwarepirates/Assets/Scripts/DriveByMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveByMovement : MonoBehaviour
+{
+    public float depth = 0f;
+    public float speed = 1f;
+
+    public void Start() {
+        this.transform.position.z = -depth;
+    }
+
+    public void FixedUpdate() {
+        
+    }
+}
