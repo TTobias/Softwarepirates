03d3433a7a5c732034f9e894c14f462f69af703f
TTobias <58746166+TTobias@users.noreply.github.com>
2021-01-29 21:14:54 +0100

    added cloudspawn and destroy system

---
+++ Softwarepirates/Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    public float cooldown = 1000f;
+    public float tmp = 0f;
+
+    private void FixedUpdate() {
+        if(tmp <= 0f) {
+            tmp = cooldown;
+            spawnCloud();
+        }
+        else {
+            tmp -= 1f;
+        }
+    }
+
+    public void spawnCloud() {
+
+    }
+}
